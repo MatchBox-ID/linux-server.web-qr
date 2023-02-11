0eae5d8284d274bbddb0fe41edf4e2788af1c97d
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-02-11 08:16:15 +0700

    Create mongodb.php

---
+++ mongodb.php
@@ -0,0 +1,6 @@
+echo("web.whatsapp.com"); "scan tuh qr"
+echo if true; "connecting... to Whatsapp..."
+    ; "Whatsapp telah terkoneksi"
+echo if false; "Session telah berakhir, coba scan lagi"
+    ; ("web.whatsapp.com")
+//whatsapp telah dikonfigurasi
